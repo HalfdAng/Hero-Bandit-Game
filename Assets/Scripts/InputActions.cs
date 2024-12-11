3ef16f618b9ef337d0d0f2806def6eba33e943b4
Mr penington <benjaminbkris@gmail.com>
2024-12-11 13:47:59 +0100

    e

--- Assets/Scripts/InputActions.cs
+++ Assets/Scripts/InputActions.cs
@@ -7,15 +7,23 @@ public class InputActions : MonoBehaviour
     public bool Jump;
     public bool Slide;
 
+<<<<<<< Updated upstream
     public bool pause;
+=======
+    public bool paused;
+>>>>>>> Stashed changes
 
     private void Update()
     {
         _inputSystem.Player.Enable();
         Jump = _inputSystem.Player.Jump.IsPressed();
         Slide = _inputSystem.Player.Slide.IsPressed();
+<<<<<<< Updated upstream
         pause = _inputSystem.Player.Pause.WasPressedThisFrame();
         pause = _inputSystem.UI.Paused.WasPressedThisFrame();
+=======
+        paused = _inputSystem.UI.Pause.WasPressedThisFrame();
+>>>>>>> Stashed changes
     }
 
     private void Awake()
