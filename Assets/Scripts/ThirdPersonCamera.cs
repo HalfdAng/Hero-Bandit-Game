57b22eada5054f7251cb78bcf064e79cb3953bcf
HalfdAng <hsgadmar@gmail.com>
2024-12-13 12:02:37 +0100

    script changes

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -12,8 +12,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
     }
 
     private void Update()
